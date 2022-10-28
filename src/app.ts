b2e6d38b1f658c241f938f7afe2fa18ac932cbb6
Renay Earnshaw <renayearnshaw@gmail.com>
2022-10-28 11:34:54 +0100

    Access the form controls in our class

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,9 @@ class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   element: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
 
   constructor() {
     this.templateElement = document.getElementById(
@@ -14,7 +17,32 @@ class ProjectInput {
       true
     );
     this.element = importedContent.firstElementChild as HTMLFormElement;
+    // Set an id to pull in formatting from the css file
+    this.element.id = 'user-input';
+    this.titleInputElement = this.element.querySelector(
+      '#title'
+    ) as HTMLInputElement;
+    this.descriptionInputElement = this.element.querySelector(
+      '#description'
+    ) as HTMLInputElement;
+    this.peopleInputElement = this.element.querySelector(
+      '#people'
+    ) as HTMLInputElement;
+
+    this.configure();
     this.attach();
+  }
+
+  private submitHandler(event: Event) {
+    event.preventDefault();
+    console.log(this.titleInputElement.value);
+  }
+
+  private configure() {
+    // We bind the submitHandler method to an event, which binds 'this' to the
+    // target of the event. The bind method overrides this, to ensure that 'this'
+    // in the submitHanlder method points to this class
+    this.element.addEventListener('submit', this.submitHandler.bind(this));
   }
 
   private attach() {
